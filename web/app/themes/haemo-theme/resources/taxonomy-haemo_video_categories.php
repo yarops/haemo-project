e8a9c985c1dc79a2a8b0a9fb6751b3ac0ca3d46d
yarops <ed.creater@gmail.com>
2025-04-22 08:57:22 +0300

    Add table view for articles

--- web/app/themes/haemo-theme/resources/taxonomy-haemo_video_categories.php
+++ web/app/themes/haemo-theme/resources/taxonomy-haemo_video_categories.php
@@ -16,27 +16,27 @@ $pagination = new \App\Modules\Paginate($wp_query);
 $content = category_description();
 
 $video_query = new \WP_Query([
-    'post_type' => 'haemo_video',
+    'post_type'      => 'haemo_video',
     'posts_per_page' => get_option('posts_per_page'),
-    'tax_query' => array(
-        array(
+    'tax_query'      => [
+        [
             'taxonomy' => 'haemo_video_categories',
-            'field' => 'slug',
-            'terms' => $term_slug,
-        ),
-    ),
+            'field'    => 'slug',
+            'terms'    => $term_slug,
+        ],
+    ],
 ]);
 
 $article_query = new \WP_Query([
-    'post_type' => 'haemo_article',
+    'post_type'      => 'haemo_article',
     'posts_per_page' => get_option('posts_per_page'),
-    'tax_query' => array(
-        array(
+    'tax_query'      => [
+        [
             'taxonomy' => 'haemo_video_categories',
-            'field' => 'slug',
-            'terms' => $term_slug,
-        ),
-    ),
+            'field'    => 'slug',
+            'terms'    => $term_slug,
+        ],
+    ],
 ]);
 
 get_header(); ?>
@@ -63,24 +63,37 @@ get_header(); ?>
         </div>
 
         <h4 class="section-title"><?php echo __('Articles', 'haemo'); ?></h4>
-        <div class="video-grid">
-            <div class="video-grid__items">
-                <?php
-                while ($article_query->have_posts()) :
-                    $article_query->the_post();
-                    ?>
 
+        <div class="card">
+            <table class="table table-striped articles-table">
+                <thead>
+                    <tr>
+                        <th><?php echo __('Title', 'haemo'); ?></th>
+                        <th><?php echo __('Date', 'haemo'); ?></th>
+                        <th><?php echo __('Actions', 'haemo'); ?></th>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                    get_template_part(
-                        'parts/video-preview',
-                        null,
-                        [
-                            'lazy' => true,
-                        ]
-                    );
-                    ?>
-                <?php endwhile; ?>
-            </div>
+                    while ($article_query->have_posts()) :
+                        $article_query->the_post();
+                        ?>
+                    <tr>
+                        <td>
+                            <a href="<?php the_permalink(); ?>" class="articles-table__link">
+                                <?php the_title(); ?>
+                            </a>
+                        </td>
+                        <td><?php the_date('d F Y') ?></td>
+                        <td class="text-muted">
+                            <a href="<?php the_permalink(); ?>" class="articles-table__action">
+                                <?php get_template_part('parts/icons/document-download'); ?>
+                            </a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
         </div>
     </main>
 <?php
