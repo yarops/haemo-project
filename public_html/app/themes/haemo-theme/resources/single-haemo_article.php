6337c8f40f9b506e1720dc0fe2851c4a94ec39c4
yarops <ed.creater@gmail.com>
2025-05-05 07:12:02 +0300

    Add article file

--- public_html/app/themes/haemo-theme/resources/single-haemo_article.php
+++ public_html/app/themes/haemo-theme/resources/single-haemo_article.php
@@ -9,7 +9,7 @@
 
 global $wp_query;
 
-$paged = get_query_var( 'paged' );
+$paged_arg = get_query_var( 'paged' );
 
 $breadcrumbs = new \App\Modules\Breadcrumbs();
 $pagination  = new \App\Modules\Paginate( $wp_query );
@@ -18,8 +18,7 @@ get_header(); ?>
 <?php
 while ( have_posts() ) :
 	the_post();
-	$videoLink = get_field( 'video_link' );
-	$diskLink  = 'https://getfile.dokpub.com/yandex/get/' . $videoLink;
+	$article_file = get_field( 'haemo_article_file' );
 	?>
 	<main class="main">
 		<div class="content-area">
@@ -27,7 +26,7 @@ while ( have_posts() ) :
 			<div class="player">
 				<div class="player__content">
 					<div class="card">
-						<h4 class="card__header"><?php echo __( 'Authors', 'haemo' ); ?></h4>
+						<h4 class="card__header"><?php echo esc_html__( 'Authors', 'haemo' ); ?></h4>
 						<div class="card__body content">
 							<?php echo apply_filters( 'the_content', get_field( 'article_authors' ) ); ?>
 						</div>
@@ -38,26 +37,30 @@ while ( have_posts() ) :
 				</div>
 				<div class="player__description">
 					<div class="card">
-						<h4 class="card__header"><?php echo __( 'Description', 'haemo' ); ?></h4>
+						<h4 class="card__header"><?php echo esc_html__( 'Description', 'haemo' ); ?></h4>
 						<div class="card__body content">
-
+                            <div class="article-description__thumb">
+							    <?php get_template_part( 'parts/icons/pdf' ); ?>
+                            </div>
 						</div>
 						<div class="card__footer">
-							<a
-								href="<?php echo $diskLink; ?>"
-								class="btn btn--primary"
-								target="_blank"
-								title="<?php echo __( 'Download', 'haemo' ); ?>"
-							>
-								<?php echo __( 'Download', 'haemo' ); ?>
-							</a>
+							<?php if ( ! empty( $article_file['url'] ) ) : ?>
+								<a
+									href="<?php echo esc_url( $article_file['url'] ); ?>"
+									class="btn btn--primary"
+									target="_blank"
+									title="<?php echo esc_html__( 'Open', 'haemo' ); ?>"
+								>
+									<?php echo esc_html__( 'Open', 'haemo' ); ?>
+								</a>
+							<?php endif; ?>
 							<a
 								href="#"
 								class="btn btn--link js-copy-link"
 								target="_blank"
-								title="<?php echo __( 'Copy link', 'haemo' ); ?>"
+								title="<?php echo esc_html__( 'Copy link', 'haemo' ); ?>"
 							>
-								<?php echo __( 'Copy link', 'haemo' ); ?>
+								<?php echo esc_html__( 'Copy link', 'haemo' ); ?>
 							</a>
 						</div>
 					</div>
